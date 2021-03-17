beb81a6d98e6b5e98abdb679700c5757b766a4ae
Jussi-Savoranta <jussi.savoranta@gmail.com>
2021-03-17 11:24:39 +0200

    axios.get() and working with the data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,13 +7,38 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        posts: []
+    }
+
+    componentDidMount () {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+            // storing first four posts
+            const posts = response.data.slice(0, 4);
+            // adding a 'author' property to all posts
+            const updatedPosts = posts.map(post => {
+                return {
+                    ...post, // spreading the data
+                    author: 'Jii' // and hard coding new property
+                }
+            })
+            this.setState({posts: updatedPosts}); // setState has to be inside this then()-method
+                             // if it was outside of it, it wouldn't be executed after then()-method
+                             // but alongside with it (execution doesn't wait for then() to finish)
+            // console.log(response);
+        });
+    }
+
     render () {
+        const posts = this.state.posts.map(post => {
+            return <Post key={post.id} title={post.title} author={post.author}/>;
+        });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
