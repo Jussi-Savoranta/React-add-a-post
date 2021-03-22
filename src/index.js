b358209f9bf14068421a49085e36f4df995bef9f
Jussi-Savoranta <jussi.savoranta@gmail.com>
2021-03-22 13:01:19 +0200

    added interceptors to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+// this is the MOST global place to put a error handling for whole app
+axios.interceptors.request.use(request => {
+    console.log(request);
+    // edit request config
+    return request;
+}, error => {
+    // this would be normally handled with catch and send to a log or something
+    console.log(error);
+    return Promise.reject(error);
+});
+
+// now we handle the response error globally and in Blog.js locally
+axios.interceptors.response.use(response => {
+    console.log(response);
+    // edit response config
+    return response;
+}, error => {
+    // this also would be handled with catch and send to a log or something
+    console.log(error);
+    return Promise.reject(error);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
